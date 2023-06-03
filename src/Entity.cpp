b5fa1536f60bfad89e1c90981299b9c5832afa2d
Willow Ciesialka <wciesialka@gmail.com>
2023-06-02 23:54:10 -0400

    updated entity

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,10 +1,10 @@
 /**
  * @file Entity.cpp
  * @brief This file contains definitions for the Entity abstract class.
- * 
+ *
  * @author Willow Ciesialka
  * @date 2023-06-02
-*/
+ */
 
 #include "Entity.hpp"
 using GolfEngine::Entity;
--- src/Entity.hpp
+++ src/Entity.hpp
@@ -1,10 +1,10 @@
 /**
  * @file Entity.hpp
  * @brief This file contains declerations of the Entity abstract class.
- * 
+ *
  * @author Willow Ciesialka
- * @date 2023-06-01
-*/
+ * @date 2023-06-02
+ */
 
 #ifndef ENTITY_H
 #define ENTITY_H
@@ -12,53 +12,59 @@
 #include "Vector2.hpp"
 #include <cmath>
 
-namespace GolfEngine {
+namespace GolfEngine
+{
     const float pi = (float)(std::acos(-1.0));
-    const float tau = pi*2.0; /* My best friend ❤️ */
+    const float tau = pi * 2.0; /* My best friend ❤️ */
 
-    class Entity {
+    class Entity
+    {
         public:
             // Entity properties
-            Entity() : position(GolfEngine::Vector2()) {};
-            Entity(Vector2 pos) : position(pos), anchored(false) {};
-            Entity(Vector2 pos, float rotation) : position(pos), anchored(false) { this->setRotation(rotation); }
-            Entity(Vector2 pos, float rotation, bool frozen) : position(pos), anchored(frozen) { this->setRotation(rotation); }
-            ~Entity() {};
+            Entity() : position(GolfEngine::Vector2::zero) {}
+            Entity(Vector2 pos) : position(pos) {}
+            Entity(Vector2 pos, float rotation) : position(pos) { this->setRotation(rotation); }
 
             /**
-             * @brief Set whether the entity is \ref anchored "anchored" or not.
+             * @brief Set the entity's position
              * 
-             * This function allows control over if the entity is anchored or not.
-             * Anchored entities are not affected by force.
-             * This may be useful for making gates, etc. controlled by buttons.
-             * 
-             * @param anchored_status True to anchor an entity, false to unanchor an entity.
+             * @param position New position of entity.
             */
-            inline void setAnchored(bool anchored_status){ this->anchored = anchored_status; }
-            
+            inline void setPosition(const Vector2 position){
+                this->position = position;
+            }
+
             /**
-             * @brief Apply a force to an entity.
-             * 
-             * @param direction Nornalized vector representing direction of force.
-             * @param force_point Point to apply force at in local space.
-             * @param magnitude Magnitude of force. 
+             * @brief Set the rotation of the entity.
+             *
+             * @param radians The new rotation of the entity in radians.
             */
-            void applyForce(GolfEngine::Vector2 direction, GolfEngine::Vector2 force_point, float magnitude);
+            inline void setRotation(float radians) { 
+                if(radians >= GolfEngine::tau){
+                    radians = fmod(radians, GolfEngine::tau); 
+                }
+                this->rotation = radians;
+            }
 
             /**
              * @brief Apply acceleration to the entity.
-             * 
+             *
              * @note This resets the current accumulated acceleration.
-            */
-            inline void applyAcceleration() {
+             */
+            inline void applyAcceleration()
+            {
                 this->velocity += this->acceleration;
                 this->acceleration = Vector2::zero;
-            };
+            }
 
             /**
              * @brief Apply velocity to the entity.
-            */
-           inline void applyVelocity() { this->position += this->velocity; }
+             */
+            inline void applyVelocity()
+            {
+                this->position += this->velocity;
+                this->velocity = Vector2::zero;
+            }
 
         private:
             // Entity properties
@@ -67,22 +73,10 @@ namespace GolfEngine {
             GolfEngine::Vector2 acceleration;
 
             /**
-             * @brief Anchored entities aren't affected by force.
-            */
-            bool anchored;
-
-            /**
-             * @brief The rotation of the entity in radians. 
-            */ 
+             * @brief The rotation of the entity in radians.
+             */
             float rotation;
-
-            /**
-             * @brief Set the rotation of the entity.
-             * 
-             * @param radians The new rotation of the entity in radians.
-            */
-            inline void setRotation(float radians) { this->rotation = fmod(radians, GolfEngine::tau); }
-    };  
+    };
 };
 
 #endif
