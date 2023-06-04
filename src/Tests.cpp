3768cee6d809aedd0641be553973fefcce9111c5
Willow Ciesialka <wciesialka@gmail.com>
2023-06-04 13:09:44 -0400

    Adding more Vector2 tests.

--- src/Tests.cpp
+++ src/Tests.cpp
@@ -12,18 +12,26 @@
 #include <cassert>
 
 void vectorTests(){
-    GolfEngine::Vector2 a(0, 0);
+    GolfEngine::Vector2 a = GolfEngine::Vector2::zero;
     GolfEngine::Vector2 b(2, 2);
-    GolfEngine::Vector2 c(0, 0);
+    const GolfEngine::Vector2 &c = GolfEngine::Vector2::zero;
     GolfEngine::Vector2 d(1, 1);
+    GolfEngine::Vector2 e(3, 4);
+    GolfEngine::Vector2 f(4, 5);
     // Comparison
     assert(a != b); 
     assert(a == c);
+    assert(&a != &c);
+    assert(&c == &GolfEngine::Vector2::zero);
     // Arithmetic
     assert(a.distanceSqr(b) == 8.0);
     assert(b.distanceSqr(a) == 8.0);
     assert((d*2) == b);
     assert((2*d) == (d*2));
+    assert((b*e) == 14.0);
+    assert((b.magnitudeSqr()) == 8.0);
+    assert((d+e) == f);
+    assert((b-b) == a);
 }
 
 void runTest(const char* test_name, Test test){
