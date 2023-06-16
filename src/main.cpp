9311e1bd73be878b1d2f4d09f683806fe3498f72
Willow Ciesialka <wciesialka@gmail.com>
2023-06-16 17:11:05 -0400

    Removed testing code.

--- src/main.cpp
+++ src/main.cpp
@@ -8,44 +8,11 @@
 
 #include "Tests.hpp"
 #define RUN_TESTS
-#include "GolfEngine/GameManagement/Scene.hpp"
-#include "GolfEngine/Rendering/Window.hpp"
-#include "GolfEngine/GameManagement/Entities/PolygonEntity.hpp"
-#include "GolfEngine/GameManagement/Entities/CircleEntity.hpp"
 
 int main(){
     #ifdef RUN_TESTS
     runTests();
     #endif
-
-    GolfEngine::Scene scene;
-    GolfEngine::Tile tile(GolfEngine::Vector2(0, 0));
-    bool success = scene.addTile(&tile);
-    std::cout << success << std::endl;
-    GolfEngine::Tile tile2(GolfEngine::Vector2(64, 0));
-    std::cout << "Origin after construction: " << tile2.getOrigin() << std::endl;
-    success = scene.addTile(&tile2);
-    std::cout << success << std::endl;
-    GolfEngine::Tile tile3(GolfEngine::Vector2(0, 64));
-    success = scene.addTile(&tile3);
-    std::cout << success << std::endl;
-    GolfEngine::Tile tile4(GolfEngine::Vector2(64, 64));
-    success = scene.addTile(&tile4);
-    std::cout << success << std::endl;
-    GolfEngine::Circle golfball_shape(4);
-    golfball_shape.setColor(0xFFFFFF);
-    GolfEngine::CircleEntity golfball(&golfball_shape, GolfEngine::Vector2(32, 32));
-    if(scene.addEntity(&golfball)){
-        std::cout << "Success!" << std::endl;
-    }
-    GolfEngine::Circle hole_shape(10);
-    hole_shape.setColor(0x0);
-    GolfEngine::CircleEntity hole(&hole_shape, GolfEngine::Vector2(100, 80));
-    scene.addEntity(&hole);
-
-    GolfEngine::Window window(&scene, 0x8b8589ff);
-
-    window.beginDisplay();
 
     return 0;
 }
