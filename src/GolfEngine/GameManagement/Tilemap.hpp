9cd2dbfa06d875ecc9a5f8a93c992e656731c6b6
Willow Ciesialka <wciesialka@gmail.com>
2023-06-16 16:41:33 -0400

    GolfEngine::Tilemap::getTileIndex now throws an error if trying to find something out-of-bounds. Updated documentation.

--- src/GolfEngine/GameManagement/Tilemap.hpp
+++ src/GolfEngine/GameManagement/Tilemap.hpp
@@ -12,6 +12,7 @@
 #include "../Rendering/Renderable.hpp"
 #include "Tile.hpp"
 #include <unordered_map>
+#include <stdexcept>
 
 namespace GolfEngine
 {
@@ -52,18 +53,34 @@ namespace GolfEngine
         bool Tilemap::addTile(GolfEngine::Tile *tile);
 
         /**
-         * @brief Convert a vector to a tile index.
+         * @brief Convert a position vector to a tile index.
+         * 
+         * Each Tile is stored with an index in the Tilemap's internal unordered map.
+         * This function will convert a position vector into an appropriate index.
          *
-         * @param vec Vector
-         * @returns Index
+         * @param vec Position of the Tile to find.
+         * @returns Index of the Tile sharing that position in the Tilemap.
+         * @throws std::out_of_range If trying to find an index corresponding to a position outside of the Tilemap's limits.
          */
         inline int getTileIndex(GolfEngine::Vector2 vec) const
         {
             // floor int
             int x = std::floor(vec.x / GolfEngine::Tile::TILE_SIZE);
             int y = std::floor(vec.y / GolfEngine::Tile::TILE_SIZE);
+            if(x < 0 || x >= this->getSideLength() || y < 0 || y >= this->getSideLength()){
+                throw std::out_of_range("Tried to find tile outside of Tilemap limits.");
+            }
             int i = x + (y * this->side_length);
             return i;
+        }
+
+        /**
+         * @brief Return the maximum number of tiles that can be placed on the Tilemap (which can be viewed as a square)'s side.
+         * 
+         * @returns The maximum side length of the Tilemap.
+        */
+        inline unsigned int getSideLength() const {
+            return this->side_length;
         }
 
     private:
