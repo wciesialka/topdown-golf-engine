a28fbfca1fe74ce192d6b8cf202d8eddc2fdf585
Willow Ciesialka <wciesialka@gmail.com>
2023-06-16 20:22:25 -0400

    Updated Golfball Entity. Added Goal Entity.

---
+++ src/GolfEngine/GameManagement/Entities/Goal.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file Goal.hpp
+ * @brief This file contains declerations for the Goal class.
+ * 
+ * @date 2023-06-16
+ * @author Willow Ciesialka
+*/
+
+#ifndef GOAL_H
+#define GOAL_H
+
+#include "../../Geometry/Vector2.hpp"
+#include "CircleEntity.hpp"
+
+namespace GolfEngine {
+    class Goal : public GolfEngine::CircleEntity {
+        public:
+            static const float RADIUS = 6.0;
+            static const int COLOR = 0x000000;
+            Goal() : GolfEngine::CircleEntity(Goal::RADIUS) {
+                this->getShape()->setColor(Goal::COLOR);
+                this->setTag("Goal");
+            }
+            
+            Goal(GolfEngine::Vector2 pos) : GolfEngine::CircleEntity(Goal::RADIUS, pos){
+                this->getShape()->setColor(Goal::COLOR);
+                this->setTag("Goal");
+            }
+    };
+}
+
+#endif
--- src/GolfEngine/GameManagement/Entities/Golfball.hpp
+++ src/GolfEngine/GameManagement/Entities/Golfball.hpp
@@ -1,24 +1,34 @@
 /**
  * @file Golfball.hpp
  * @brief This file contains declerations for the Golfball Entity class.
- * 
+ *
  * @date 2023-06-13
  * @author Willow Ciesialka
-*/
+ */
 
 #ifndef GOLFBALL_H
 #define GOLFBALL_H
 
+#include "../../Geometry/Vector2.hpp"
 #include "CircleEntity.hpp"
 
-namespace GolfEngine {
-    class Golfball : public GolfEngine::CircleEntity {
-        Golfball() : GolfEngine::CircleEntity(4.0) {
-            this->getShape()->setColor(0xFFFFFF);
+namespace GolfEngine
+{
+    class Golfball : public GolfEngine::CircleEntity
+    {
+    public:
+        static const float RADIUS = 4.0;
+        static const int COLOR = 0xFFFFFF;
+
+        Golfball() : GolfEngine::CircleEntity(Golfball::RADIUS)
+        {
+            this->getShape()->setColor(Golfball::COLOR);
             this->setTag("Golfball");
         }
-
-        virtual void onCollision(Entity* collider);
+        Golfball(GolfEngine::Vector2 pos) : GolfEngine::CircleEntity(Golfball::RADIUS){
+            this->getShape()->setColor(Golfball::COLOR);
+            this->setTag("Golfball");
+        }
     };
 }
 
